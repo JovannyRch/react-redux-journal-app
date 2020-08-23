bbded190c4b775593047c772f4425a381805f697
Farid Esquivel <esquivelfarid@gmail.com>
2020-08-22 22:20:37 -0500

    first commit

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'auth-login':
+
+            break;
+
+        default:
+            return state;
+    }
+}
